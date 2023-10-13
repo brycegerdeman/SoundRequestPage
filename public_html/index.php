e2acfc9fc6998071e7e94400a9eade619a792755
Jason Moore <mooreja10@appstate.edu>
2023-10-12 20:23:33 -0400

    Added stylesheet.

--- public_html/index.php
+++ public_html/index.php
@@ -2,20 +2,34 @@
 
 <head>
 
+	<link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
 
-<form enctype="multipart/form-data" method="post" action="submitted.php">
-	
-	<label for="banner">Banner ID:</label>
-	<input type="text" name="banner">
-	<label for="">Upload your sound:<br>(Accepted filetype: mp3)</label>
-	<input type="hidden" name="MAX_FILE_SIZE" value="500000">
-	<input type="file" name="soundreq" accept=".mp3">
-	<input type="submit">
+<div id="page">
+		
+	<form id="sndform" enctype="multipart/form-data" method="post" action="submitted.php">
+		
+		<div id="inputblock">
+			<div class="inputwrap">
+				<label for="banner">Banner ID:</label><br>
+				<input type="text" name="banner">
+			</div>
+			<div class="inputwrap">
+				<label for="">Upload your sound:<br>
+				<span id="hint">(Accepted filetype: mp3)</span></label>
+				<br>
+				<input type="hidden" name="MAX_FILE_SIZE" value="500000">
+				<input id="fileupload" type="file" name="soundreq" accept=".mp3">
+			</div>
+			<input id="submit" type="submit">
+		</div>
 
-</form>
+	</form>
+
+</div>
 
 </body>
 
